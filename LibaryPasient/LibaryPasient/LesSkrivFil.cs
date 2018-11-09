8c9b586902b363e7e2b35d392c3e905dbd6dbb18
benska13 <benskare@gmail.com>
2018-11-09 12:04:49 +0100

    LesSkriv fil

---
+++ LibaryPasient/LibaryPasient/LesSkrivFil.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Web.Script.Serialization;
+
+namespace LibaryPasient
+{
+    public class LesSkrivFil
+    {
+        public static BindingList<ListPasient> LesFraFil(string filnav)
+        {
+            BindingList<ListPasient> _p = null;
+            
+            
+            try
+            {
+                var serilazer = new JavaScriptSerializer();
+                string data = File.ReadAllText(filnav);
+
+                _p = serilazer.Deserialize<BindingList<ListPasient>>(data);
+
+            }
+            catch (SerializationException e)
+            {
+               
+                throw;
+            }
+
+            return _p;
+        }
+
+        public static void SkrivTilFil(BindingList<ListPasient> _p, string filnav)
+        {
+            try
+            {
+                var serilazer = new  JavaScriptSerializer();
+                string data = serilazer.Serialize(_p);
+                File.WriteAllText(filnav,data);
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+    }
+}
